e65fab68ec05ab41f579a582ffdc751fe4909272
“chloedillingham” <chloeldillingham@gmail.com>
2023-12-22 23:47:36 -0800

    allow for chat history

--- openai-api.py
+++ openai-api.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 import os
 from openai import *
-from flask import Flask, render_template, request
+from flask import Flask, session, render_template, request, jsonify
+from flask_session import Session
+import secrets
 
 # # Get the API key from the environment variables - dont adjust
 load_dotenv()
@@ -11,40 +13,48 @@ if api_key is None:
 client = OpenAI(api_key=api_key)
 
 app = Flask(__name__)
+# Configure the secret key for sessions
+app.config['SECRET_KEY'] = secrets.token_hex(16)  
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 
 @app.route('/', methods=['GET'])
 def index():
-    # Render the main page
+    session['conversation'] = []
     return render_template('index.html')
 
 @app.route('/generate', methods=['POST'])
 def generate():
-    # Get data from form
-    role = request.form['role']
-    user_message = request.form['user_message']
-    model = request.form['model']
+    data = request.get_json()
 
-    # Set up the messages for the completion request
-    messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": role, "content": user_message}
-    ]
+    # Retrieve the user message and model from the request
+    user_message = data['message']
+    model = data.get('model', 'text-davinci-003')  # Default to a specific model
+
+    # Initialize conversation history if it doesn't exist
+    if 'conversation' not in session:
+        session['conversation'] = []
+
+    # Append the user's message to the conversation history
+    session['conversation'].append({"role": "user", "content": user_message})
 
     # Call the OpenAI API
     try:
         response = client.chat.completions.create(
-            model=model,
-            messages=messages
+            model='gpt-4',
+            messages=session['conversation']
         )
     except APIError as e:
         # Handle API errors gracefully
-        return render_template('index.html', generated_text=f"Error: {str(e)}")
+        return jsonify({'error': str(e)})
 
-    # Extract the assistant's response
+    # Extract the assistant's response and append it to the conversation
     generated_text = response.choices[0].message.content
+    print(generated_text)
+    session['conversation'].append({"role": "assistant", "content": generated_text})
 
-    # Render the page with the generated text
-    return render_template('index.html', generated_text=generated_text)
+    # Return the generated text as JSON
+    return jsonify({'generated_text': generated_text})
 
 if __name__ == '__main__':
     app.run(debug=True)
